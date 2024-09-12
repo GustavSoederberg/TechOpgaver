9a654271cc19d81e2647f65eb09cf3ba8ec84e10
GustavSoederberg <144337665+GustavSoederberg@users.noreply.github.com>
2024-09-12 13:02:45 +0200

    Inital commit

---
+++ src/main/java/com/example/techopgaver/TechOpgaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.techopgaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechOpgaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechOpgaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techopgaver/TechOpgaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techopgaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechOpgaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
